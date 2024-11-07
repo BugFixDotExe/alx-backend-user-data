becfe10d211f9c51cf838ceb4c26f111c49ea09d
BugFixDotExe <bugfix.exe@gmail.com>
2024-11-07 10:31:44 +0100

    used chatgtp to solve this

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,23 +9,5 @@ import re
 import logging
 
 
-def filter_datum(
-                fields: List[str],
-                redaction: str,
-                message: str,
-                separator: str):
-    '''
-        filter_datum: a function that returns a new string with
-        it's to be redacted part, redacted
-        Args:
-            fields: A list of fields to look out for in the sting
-            redaction: The readaction to be applied
-            message: The message to look into
-            separtor: what should be used to end the redacted string
-        Returns:
-            A string with the redaction take
-    '''
-    for field in fields:
-        pattern = fr'({field}=)([^;]*?);'
-        message = re.sub(pattern, fr'\1{redaction}{separator}', message)
-    return message
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    return re.sub(rf"({'|'.join(fields)})=([^;]*)", lambda m: f"{m.group(1)}={redaction}", message)
