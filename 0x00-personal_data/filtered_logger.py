d0942a293b19f86d2c5cdb51b388ef9c6f5b62e9
BugFixDotExe <bugfix.exe@gmail.com>
2024-11-07 10:38:59 +0100

    white towel 🏳️

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,15 @@
-import re
+#!/usr/bin/env python3
+'''
+A module that contains the function
+filter_datum that reuturns the log message
+obsfuscated.
+'''
 from typing import List
+import re
+import logging
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    pattern = fr"({'|'.join(fields)})=([^;]*?);"
-    return re.sub(pattern, fr"\1{redaction};", message)
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str):
+    for field in fields:
+        message = re.sub(fr'({field}=)([^;]*?);', fr'\1{redaction}{separator}', message)
+    return message
