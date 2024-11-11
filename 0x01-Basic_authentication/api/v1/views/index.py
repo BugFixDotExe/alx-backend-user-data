99bb716a3eebbe8f51b3d49dbad83067ccb162cd
BugFixDotExe <bugfix.exe@gmail.com>
2024-11-11 10:53:15 +0100

    Solutions to problem 0-2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,22 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """GET  /api/v1/unauthorized
+    Returns:
+        - an error message, if not auntheticated
+    """
+    abort(401)
+    return jsonify({"error": "Unauthorized"}) 
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """GET /api/v1/forbidden
+    Returns:
+        - an error message, for forbidden
+    """
+    abort(403)
+    return jsonify({"error": "Forbidden"})
