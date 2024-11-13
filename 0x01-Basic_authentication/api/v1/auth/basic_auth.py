81f645ab4f82047d14311b27c55362739496d377
BugFixDotExe <bugfix.exe@gmail.com>
2024-11-13 10:16:46 +0100

    solution to porblem 7, testing time

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,24 @@ class BasicAuth(Auth):
     '''
     BasicAuth a class that inheris from the Auth class
     '''
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        '''
+        extract_base_64: a function that returns the
+        Base64 part of the Authorization
+        header for a Basic Authentication
+        Args:
+            authorization_header: A header having the Authorization field
+        Returns:
+            A string of base64, after converting the authroiation_header
+        '''
+        if authorization_header is None:
+            return None
+        if issubclass(type(authorization_header), str) is False:
+            return None
+        split_header = authorization_header.split()
+        if len(split_header) <= 1:
+            return None
+        if "Basic" not in split_header[0]:
+            return None
+        return split_header[1]
