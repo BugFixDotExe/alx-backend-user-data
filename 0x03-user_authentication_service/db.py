080d58ea100af09cd8ff737b52b0403a5c8194b1
BugFixDotExe <bugfix.exe@gmail.com>
2024-11-19 09:15:29 +0100

    correction to the inital code

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,9 +1,7 @@
+#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
-from sqlalchemy.exc import (
-    ArgumentError, InvalidRequestError,
-    NoReferenceError, NoResultFound)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
@@ -35,33 +33,14 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         '''
         add_user: A method that adds a user to the db
-        Args:
-            email: an email
-            hashed_password: a hashed string
-        Returns:
-            A type of User
-        '''
+            Args:
+                email: an email
+                hashed_password: a hashed string
+            Returns:
+                A type of User
+            '''
         session = self._session
         new_user = User(email=email, hashed_password=hashed_password)
         session.add(new_user)
         session.commit()
         return new_user
-
-    def find_user_by(self, email: str) -> User:
-        '''
-        find_user_by: A function that queries for a user
-        Args:
-            email: the email of the user to query for
-        Returns:
-            A type User if found
-            or raises NoResultFound
-            and if  input is invalid an InvalidRequestError
-            is raised
-        '''
-        session = self._session
-        try:
-            return session.query(User).filter_by(email=email).one()
-        except NoResultFound:
-            raise NoResultFound
-        except InvalidRequestError:
-            raise InvalidRequestError
